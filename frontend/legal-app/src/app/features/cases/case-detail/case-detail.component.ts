96e341852cf90b71cee6925396af5be02fac5651
jacoballeza87 <jacoballeza87@gmail.com>
2026-08-18 05:01:57 -0600

    Delete frontend/legal-app/src/app/features/cases/case-detail/case-detail.component.ts

--- frontend/legal-app/src/app/features/cases/case-detail/case-detail.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-case-detail',
-  template: '<div class="case-detail-placeholder">Case detail is not available in this build.</div>'
-})
-export class CaseDetailComponent {}
